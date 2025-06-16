7240c45b9255d611c6dc066ff2e976518bce5210
luis-braghin <luisz.braghin@hotmail.com>
2025-06-16 17:42:00 -0300

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ eventos = [
 
 destaque_comercial = {
     "nome": "Comercial Gabrielle Figueira - Regional Lilian Cafarella",
-    "motivo": "🚀 Receitas 🚀"
+    "motivo": "🚀 Top Receitas 🚀"
 }
 # ============================================= #
 
