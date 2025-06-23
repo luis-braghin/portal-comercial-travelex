ffeef47fee2207eb7a407afe2cfcd3ecdfd6b1c2
luis-braghin <luisz.braghin@hotmail.com>
2025-06-23 10:58:05 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from streamlit_option_menu import option_menu
 import base64
 
 # ========= CONFIGURAÇÕES DE CONTEÚDO ========= #
-mensagem_atualizacao = "🔔 Atualização: Estamos prestes a lançar nossa plataforma de CRM!
+mensagem_atualizacao = "🔔 Atualização: Estamos prestes a lançar nossa plataforma de CRM!"
 
 eventos = [
     "🖙️ Divulgação do BCB Focus (Expectativas do mercado para câmbio e inflação) - 23 de Junho",
